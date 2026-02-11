16c51519460ed04eaa4c0371dc3689859012268c
Tashreeful Islam <tashreeful12@gmail.com>
2026-02-11 08:55:59 +0600

    Add first 800-rated problem solution (41A)
    Implement solution for Codeforces problem 41A - Translation.

---
+++ by-rating/800/41A_Translation.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: A - Translation
+Link: https://codeforces.com/problemset/problem/41/A
+Rating: 800
+Author: Tashreeful Islam
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+    if (s == t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
