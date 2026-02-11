c0df77e6ad891495cf97740af136a5e9c7542376
Tashreeful Islam <tashreeful12@gmail.com>
2026-02-11 09:05:44 +0600

    Add solution for 50A - Domino piling problem

---
+++ by-rating/800/50A_Domino_piling.cpp
@@ -0,0 +1,19 @@
+/*
+Problem: 50A - Domino piling
+Link: https://codeforces.com/problemset/problem/50/A
+Rating: 800
+Author: Tashreeful Islam
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin>>m>>n;
+
+    int sum = (m*n) / 2;
+    cout<<sum<<endl;
+    return 0;
+}
